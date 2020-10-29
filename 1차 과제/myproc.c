57e231ce5db8fcba80bc49944a890b5608cddb84
unknown <noparkee@gmail.com>
2020-10-29 15:02:06 +0900

    edit lkm

--- 1차 과제/myproc.c
+++ 1차 과제/myproc.c
@@ -29,7 +29,7 @@ static int my_open(struct inode *inode, struct file *file){
 static ssize_t my_read(struct file * file, char __user *buf, size_t count, loff_t * ofs)
 {	
 	BlockQueue block = bqueue[idx];
-	sprintf(buf, "index: %d || time: %ld || fs_name: %s || block_number: %u\n", idx, block.time, block.fs_name, block.block_number);
+	int len  = sprintf(buf, "index: %d || time: %ld || fs_name: %s || block_number: %u \n", idx, block.time, block.fs_name, block.block_number);
 	printk("block_number: %u\n", block.block_number);
 	idx ++;
 	printk("%d\n", idx);
@@ -38,7 +38,7 @@ static ssize_t my_read(struct file * file, char __user *buf, size_t count, loff_
 	return 0;
 	}
     else
-         return count;
+         return len;
 }
 
 
